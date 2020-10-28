236a9c3ac78874c56272df6041e201ba6648ae1e
Frank Septillion <frankseptillion@msn.cn>
2020-10-28 15:20:26 +0800

    Initial Commit

---
+++ Detect Resolution iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Detect Resolution iOS
+//
+//  Created by 王跃琨 on 2020/10/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Detect Resolution iOS/Detect_Resolution_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Detect_Resolution_iOSApp.swift
+//  Detect Resolution iOS
+//
+//  Created by 王跃琨 on 2020/10/28.
+//
+
+import SwiftUI
+
+@main
+struct Detect_Resolution_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
